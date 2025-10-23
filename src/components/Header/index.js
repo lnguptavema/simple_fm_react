90a1b22f3bec9c62416ab5d881fcb3266cdf6626
lnguptavema <nareshgupta2294@gmail.com>
2025-10-23 14:14:24 +0000

    first commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,45 +2,56 @@ import './index.css'
 
 const Header = () => (
   <div className="mainContainer">
-    <h1 className="name">Gupta Projects</h1>
+    <h1 className="name">Gupta Radios</h1>
+
+    <div className="divContainer">
+      <a
+        href="https://onlineradiofm.in/andhra-pradesh/vijayawada/fm-rainbow"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="img"
+          src="https://onlineradiofm.in/assets/image/radio/180/fmrainvijayawada1.jpg"
+        />
+        <p>102.2 Rainbow</p>
+      </a>
+      <a
+        href="https://onlineradiofm.in/andhra-pradesh/vijayawada/red"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="  https://onlineradiofm.in/assets/image/radio/180/red-fm15.webp" />
+
+        <p> 93.5 Redfm</p>
+      </a>{' '}
+      <a
+        href="https://www.radioindia.in/kushi-fm"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="https://static.mytuner.mobi/media/tvos_radios/485/kushi-fm.da9042f7.png" />
+        <p>Kushi FM</p>
+      </a>{' '}
+      <a
+        href="https://onlineradiofm.in/andhra-pradesh/vijayawada/vividh-bharati"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="https://onlineradiofm.in/assets/image/radio/180/vbstelugu.jpg" />
+        <p>Vivid Bharathi FM</p>
+      </a>
+      <a
+        href="https://onlineradiofm.in/andhra-pradesh/vijayawada/mirchi"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="https://onlineradiofm.in/assets/image/radio/180/radio-mirchi.webp" />
+        <p>98.3 Mirchi</p>
+      </a>
+    </div>
     <h1>HTML Projects</h1>
     <h1>ReactJS Projects</h1>
-    <ul>
-      <li className="listContainer">
-        <img
-          className="listlogo"
-          src="https://assets.ccbp.in/frontend/content/react-js/simple-todos-output.gif"
-          alt="ii"
-        />
-        <h1 className="nameProject">Delete List </h1>
-        <div>
-          {' '}
-          <p>Live Link:</p>{' '}
-          <a
-            href="https://deletelistvlng.ccbp.tech/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>https://deletelistvlng.ccbp.tech/</p>
-          </a>
-        </div>
-      </li>
-      <li className="listContainer">
-        <img
-          className="listlogo"
-          src="https://assets.ccbp.in/frontend/content/react-js/google-search-suggestions-output.gif"
-          alt="ii"
-        />
-        <p className="nameProject">Goggle suggestion</p>
-        <a
-          href="https://suggestionsvlng.ccbp.tech/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p>Live Link: https://suggestionsvlng.ccbp.tech/</p>
-        </a>
-      </li>
-    </ul>
   </div>
 )
 
