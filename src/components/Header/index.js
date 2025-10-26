f47a59ae9c25fa562a11ce20506db3532859ba87
lnguptavema <nareshgupta2294@gmail.com>
2025-10-26 21:42:13 +0530

    updating commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,13 +17,12 @@ const Header = () => (
         <p>102.2 Rainbow</p>
       </a>
       <a
-        href="https://onlineradiofm.in/andhra-pradesh/vijayawada/red"
+        href="https://onlineradiofm.in/andhra-pradesh/vijayawada/vividh-bharati"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src="  https://onlineradiofm.in/assets/image/radio/180/red-fm15.webp" />
-
-        <p> 93.5 Redfm</p>
+        <img src="https://onlineradiofm.in/assets/image/radio/180/vbstelugu.jpg" />
+        <p>Vivid Bharathi FM</p>
       </a>{' '}
       <a
         href="https://www.radioindia.in/kushi-fm"
@@ -34,13 +33,40 @@ const Header = () => (
         <p>Kushi FM</p>
       </a>{' '}
       <a
-        href="https://onlineradiofm.in/andhra-pradesh/vijayawada/vividh-bharati"
+        href="https://indianradio.in/station/eenadu-fm/#google_vignette"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <img src="https://onlineradiofm.in/assets/image/radio/180/vbstelugu.jpg" />
-        <p>Vivid Bharathi FM</p>
-      </a>
+        <img src="https://static.wixstatic.com/media/4365a2_3922d44317be49e39f0d9a4fbf85388f~mv2_d_1439_1439_s_2.jpg/v1/fill/w_300,h_298,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Eenadu.jpg" />
+        <p>Enadu FM</p>
+      </a>{' '}
+      <a
+        href="https://www.radioindia.in/ap-9-fm#google_vignette"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="https://static.mytuner.mobi/media/tvos_radios/864/ap-9-fm.809abf34.png" />
+
+        <p> AP9 fm</p>
+      </a>{' '}
+      <a
+        href="https://onlineradiofm.in/andhra-pradesh/vijayawada/all-india-air-akashvani"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="https://onlineradiofm.in/assets/image/radio/180/air_live.webp" />
+
+        <p>Akasvani fm</p>
+      </a>{' '}
+      <a
+        href="https://onlineradiofm.in/andhra-pradesh/vijayawada/red"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="https://onlineradiofm.in/assets/image/radio/180/red-fm15.webp" />
+
+        <p> 93.5 Redfm</p>
+      </a>{' '}
       <a
         href="https://onlineradiofm.in/andhra-pradesh/vijayawada/mirchi"
         target="_blank"
@@ -50,8 +76,6 @@ const Header = () => (
         <p>98.3 Mirchi</p>
       </a>
     </div>
-    <h1>HTML Projects</h1>
-    <h1>ReactJS Projects</h1>
   </div>
 )
 
